4128362c38e1d6b2795b7321c76ef3126fc901d4
Wouter <wouter075@gmail.com>
2022-11-28 12:45:00 +0100

    Day 4, done!

--- day4.py
+++ day4.py
@@ -4,6 +4,7 @@ with open('day4.txt') as f:
     lines = [line.rstrip() for line in f]
 
 part1 = 0
+part2 = 0
 for line in lines:
     c = dict(Counter(line.split(" ")))
     for x in c.values():
@@ -12,10 +13,23 @@ for line in lines:
     else:
         part1 += 1
 
-for line in lines:
-    c = dict(Counter(line.split(" ")))
-    for p2 in list(c):
-        print(p2)
-        print(Counter(p2).most_common())
-    print("-"*20)
-# print(part1)
+        # for part 2
+        anagram = False
+        anagram_list = []
+        for p2 in list(c):
+            anagram_list.append(sorted(Counter(p2).most_common()))
+
+        for x in range(0, len(anagram_list)):
+            for y in range(0, len(anagram_list)):
+                if x != y:
+                    if anagram_list[x] == anagram_list[y]:
+                        anagram = True
+                        break
+        if anagram:
+            print(f'[Not valid] {line}')
+        else:
+            print(f'[Valid] {line}')
+            part2 += 1
+
+print(part1)
+print(part2)
